3160ca1f4a784c00efbc03371a8e7cfb13fc8ed3
ductoan2732000 <ductoan2732000@gmail.com>
2023-04-16 21:24:17 +0700

    add ui framework

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { createApp } from 'vue'
+// pinia store
 import { createPinia } from 'pinia'
+// element-ui
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
 
-import './assets/main.css'
+import '@/assets/scss/styles.scss'
 
 const app = createApp(App)
+
+app.use(ElementPlus)
 
 app.use(createPinia())
 app.use(router)
